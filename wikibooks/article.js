3ec41d1cb029086de225ea1d40fc892fd21894d1
Willi Raschkowski <willi@raschkowski.com>
2016-10-03 03:39:32 +0200

    Implement sections

--- wikibooks/article.js
+++ wikibooks/article.js
@@ -1,91 +1,105 @@
-var request = require('request');
-var cheerio = require('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
 
 
-var options = {decodeEntities: false};
+const options = {decodeEntities: false};
 
-var Article = exports.Article = function (html) {
-  var $ = this.$ = cheerio.load(html, options);
-  this.body = $('#mw-content-text');
-  this.title = $('#section_0').text();
+exports.Article = class Article {
+	constructor(html) {
+		const $ = this.$ = cheerio.load(html, options);
+		this.body = $('#mw-content-text');
+		this.title = $('#section_0').text();
+	}
+
+	static fromUrl(url) {
+		return new Promise((resolve, reject) => {
+			request(url, (error, response, body) => {
+				if (!error && response.statusCode == 200) {
+					resolve(new Article(body));
+				}
+				reject(error);
+			});
+		});
+	}
+
+	kindleHTML() {
+		const $ = cheerio.load(emptyDom(this.title), options);
+		const body = $('body');
+		const cleanBody = clean(this.body.html());
+		body.append(`<h1 id="abstract">${this.title}</h1>`);
+		body.append(cleanBody);
+		return $.html();
+	}
+}
+
+const clean = function(body) {
+	// Create a copy of the body
+	const $ = cheerio.load(body, options);
+	// Remove table of contents
+	$('div#toc').remove();
+	// Remove "if you mean..."
+	$('div.hatnote').remove();
+	// Remove "edit" links
+	$('a.edit-page').parent('span').remove();
+	// Remove empty divs
+	$('div.indicator').remove();
+	// Remove noscript tag
+	$('noscript').remove();
+	// Move span content in header up to headers
+	$('span.mw-headline').each((i, el) => {
+		$(el).parent()
+			.attr('id', $(el).attr('id'))
+			.text($(el).text());
+	});
+	// Remove links to non-existent articles
+	$('a.new').each((i, el) => $(el).replaceWith($(el).text()));
+	// Remove titles
+	$('*').removeAttr('title');
+
+	return $.html();
 };
 
-Article.fromUrl = function (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        resolve(new Article(body));
-      }
-      reject(error);
-    });
-  });
-};
+/**
+ * Takes a clean() HTML and iterates headers to build a section tree.
+ * H2 tags are considered root level.
+ * Higher h-tags are pushed in the 'sections' of the last lower tag.
+ */
+const sections = function(body) {
+	const $ = cheerio.load(body, options);
+	const root = [{
+		name: 'Abstract',
+		id: '#abstract',
+		sections: []
+	}];
 
-Article.prototype.clean = function () {
-  var $ = cheerio.load(this.body.html(), options);
-  // TODO: Tags
-  // 'div', 'p', 'b', 'a', 'sup', 'h2',
-  // 'span', 'i', 'blockquote', 'ul', 'li',
-  // 'ol', 'cite', 'q', 'table', 'tr', 'td',
-  // 'noscript', 'img'
-  $('div#toc').remove();
-  $('div.hatnote').remove();
-  $('a.edit-page').parent('span').remove();
-  $('div.indicator').remove();
-  $('noscript').remove();
-  $('*').removeAttr('title');
-  $('span.mw-headline').each((i, el) => {
-    $(el).parent()
-      .attr('id', $(el).attr('id'))
-      .text($(el).text());
-  });
-  $('a.new').each((i, el) => $(el).replaceWith($(el).text()));
+	$('h2, h3, h4, h5, h6').each((i, el) => {
+		const sec = $(el);
+		const currentHValue = +sec.get(0).tagName.slice(1);
+		let depth = currentHValue - 2;
+		let superSection = root;
+		// While the current depth is higher than 2,
+		// go to right-most section and select select its subsections
+		for (; depth > 0; depth--) {
+			superSection = superSection[superSection.length - 1].sections;
+		}
 
-  return $.html();
-};
+		const currentSection = {
+			name: sec.text(),
+			sections: [],
+			id: sec.attr('id')
+		};
+		superSection.push(currentSection);
+	});
+	return root;
+}
 
-Article.prototype.toKindleHtml = function () {
-  var $ = cheerio.load(emptyDom(this.title), options);
-  var sections = [{
-    name: 'Abstract',
-    id: '#abstract',
-    subsections: []
-  }];
-
-  var body = $('body');
-  body.append(`<h1 id="abstract">${this.title}</h1>`);
-
-  this.body.children()
-    .filter(function () {
-      // Get only sections themselves
-      return /mf-section/.test($(this).attr('class'))
-    })
-    .each(function (i) {
-      // Append content of each section to new dom
-      var sec = $(this);
-      var section = {
-        name: $('.mw-headline', sec.prev()).text(),
-        id: $('.mw-headline', sec.prev()).attr('id')
-      };
-      if (i > 0)
-        body.append(`<h2 id="${section.id}">${section.name}</h2>`);
-
-      sec.children().each(function () {
-        var elem = $(this);
-        elem.appendTo(body);
-      });
-    });
-
-  return $.html();
-};
-
-function emptyDom(title) {
-  return `
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>${title}</title>
-  </head>
-  <body></body>
-</html>`
+const emptyDom = function(title) {
+	return `
+		<html>
+		<head>
+		<meta charset="UTF-8">
+		<title>${title}</title>
+		</head>
+		<body></body>
+		</html>`
 }
--- wikibooks/main.js
+++ wikibooks/main.js
@@ -1,25 +1,25 @@
-var {Article} = require('./article.js');
-var pretty = require('pretty');
-var fs = require('fs');
+const {Article} = require('./article.js');
+const pretty = require('pretty');
+const fs = require('fs');
 
-var urls = [
-  'https://en.m.wikipedia.org/wiki/List_of_cognitive_biases',
-  'https://en.m.wikipedia.org/wiki/Hanlon%27s_razor',
-  'https://en.m.wikipedia.org/wiki/Occam%27s_razor',
-  'https://de.m.wikipedia.org/wiki/Eurokrise'
+const urls = [
+	// 'https://en.m.wikipedia.org/wiki/List_of_cognitive_biases',
+	// 'https://en.m.wikipedia.org/wiki/Hanlon%27s_razor',
+	'https://en.m.wikipedia.org/wiki/Occam%27s_razor',
+	// 'https://de.m.wikipedia.org/wiki/Eurokrise'
 ];
 // Article.fromUrl('Occam%27s_razor')
 
 urls.forEach(url => {
-  Article.fromUrl(url)
-    .then(function (a) {
-      // var html = a.toKindleHtml();
-      // console.log(pretty(html));
-      var cleanedHtml = a.clean();
-      // console.log(pretty(cleanedHtml));
-      fs.writeFile('../examples/output.html', pretty(cleanedHtml));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+	Article.fromUrl(url)
+		.then(a => {
+			const html = a.kindleHTML();
+			//console.log(pretty(html));
+			//const cleanedHtml = a.clean();
+			//console.log(pretty(cleanedHtml));
+			fs.writeFile('../examples/output.html', pretty(html));
+		})
+		.catch(error => {
+			console.log(error);
+		});
 });
